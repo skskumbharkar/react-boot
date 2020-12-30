37112ba09caef6d36741aede4f074d31d9c8c0ee
sachin <skskumbharkar@gmail.com>
2020-12-30 21:04:35 +0530

    Conflict resolution

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ export const App: React.FC<AppProps> = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
+                <h1>Hello World</h1>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>

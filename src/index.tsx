633ef7bbd71793f8f16f8d2579e730aacfc5f8cf
sachin <skskumbharkar@gmail.com>
2020-12-31 14:00:54 +0530

    Go through create react app docs
    Document highlight pointer from create react app docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import './index.scss';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const onLoad = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>,
-        document.getElementById('root'),
-    );
+    const rootElement = document.getElementById('root');
+    if (rootElement && rootElement.hasChildNodes()) {
+        hydrate(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>,
+            rootElement,
+        );
+    } else {
+        render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>,
+            rootElement,
+        );
+    }
 };
 
 setTimeout(onLoad, 5000);

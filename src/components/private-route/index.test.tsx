3d197d7e4a2691775cb1acf5523d78e1f861c779
sachin <skskumbharkar@gmail.com>
2021-01-19 13:45:16 +0530

    Unit test fix for private routes.

--- src/components/private-route/index.test.tsx
+++ src/components/private-route/index.test.tsx
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createTestStore } from 'utils/mock/store';
 import { history } from 'stores';
+import { Route, Switch } from 'react-router';
 import { ProtectedRoute } from './index';
+import routes from '../../static/routes';
 
 let store: any;
 beforeEach(() => {
@@ -15,10 +17,14 @@ test('Render test for private route component', async () => {
     // Arrange
 
     // Action
+    // Note Tested internal component of protected route here as can't pass another custom component as child to router
+    // It will throw error: thrown: "Could not find router reducer in state tree, it must be mounted under \"router\""
     const component = render(
         <Provider store={store}>
             <ConnectedRouter history={history}>
-                <ProtectedRoute key="test" />
+                <Switch>
+                    <Route exact path="/home" render={() => <div />} />
+                </Switch>
             </ConnectedRouter>
         </Provider>,
     );
